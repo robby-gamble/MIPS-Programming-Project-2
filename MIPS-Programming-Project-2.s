fab432534fe85b47a4fed3adadf90559667687de
Robby Gamble <robby.gamble2017@gmail.com>
2019-11-14 21:04:05 -0500

    I Created a loop to count the amount of characters in my string.

--- MIPS-Programming-Project-2.s
+++ MIPS-Programming-Project-2.s
@@ -19,14 +19,37 @@ li $a1, 1001 #allocating byte space for string to be stored
 syscall #executing command
 
 
+addi $t7,$t7,0      #initializing my counter
+addi $t6,$t6,1      #1: continues loopiing, 0: haults the loop
+addi $t5,$t5,1      #exponent of base- 33
+addi $t4,$t4,0      #length of my string
+
+
+
 #Get rid of my trailing Spaces
 
 trailSpaces:
+jal lengthOfString #First I'll need to figure out the lenght of my string.
+
 
 
 Invalid:
 
+
+
 lengthOfString:
+li $t4,0                          #setting the intital value of my string's length to 0 string length
+count: li $t3,0
+addu $t3,$t3,$t7                  #$t3 = my iterator
+addu $t3,$t3,$a1                  #$t3 = position in my string
+lbu $a2,($t3)                     #loads position to $a2
+beq $a2,0,exit                    #check for a NULL value
+addi $t7,$t7,1                    #increment my iterator by 1
+addi $t4,$t4,1                    #increaseing the value of my string length by 1
+j count                           #restarts my count loop
+exit: jr $ra
+
+
 
 Calcuate:
 
